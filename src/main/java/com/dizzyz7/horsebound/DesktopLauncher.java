75424457971272de52362169098af56d72d97132
Dimash Janibekov <99655528+DizzyZ7@users.noreply.github.com>
2026-08-01 01:21:11 +0300

    Install local crash reporting before game startup

--- src/main/java/com/dizzyz7/horsebound/DesktopLauncher.java
+++ src/main/java/com/dizzyz7/horsebound/DesktopLauncher.java
@@ -4,16 +4,27 @@ package com.dizzyz7.horsebound;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
+import java.util.Arrays;
+
 public final class DesktopLauncher {
     private DesktopLauncher() {
     }
 
     public static void main(String[] args) {
+        CrashReportService.installDefaultHandler();
+        BuildInfo buildInfo = BuildInfo.current();
+
+        if (Arrays.asList(args).contains("--version")) {
+            System.out.println(buildInfo.diagnosticLabel());
+            System.out.println("Created by " + AuthorInfo.CREATOR);
+            return;
+        }
+
         SettingsRepository settingsRepository = new SettingsRepository();
         GameSettings settings = settingsRepository.load();
 
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle(AuthorInfo.GAME_NAME + " | Created by DizZyZ7");
+        config.setTitle(buildInfo.diagnosticLabel() + " | Created by DizZyZ7");
         config.setWindowedMode(1280, 720);
         config.setResizable(true);
         config.useVsync(settings.vsync());
